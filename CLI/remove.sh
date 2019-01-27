ad094878d29f53ac41d90187a0d1c4a3e79ca6c2
Merculous <pettit.brandon12@gmail.com>
2019-01-26 19:40:50 -0800

    yeetus

--- CLI/remove.sh
+++
@@ -1,7 +0,0 @@
-
-echo "Removing shit..."
-
-rm -rf applelogo ramdisk devicetree kernelcache* Firmware *.plist *.dmg *.raw *.prepatched *.patched *.dif *.ipsw *.json *.dfu *.img3 .fseventsd .DS_Store
-
-echo "Done :P"
-
